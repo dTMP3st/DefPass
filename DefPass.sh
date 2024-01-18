fc0b698653bdd21d14b7f69d7d393de8c13bcd6d
dTMP3st <ThiagoMirandaDePaula@gmail.com>
2024-01-18 15:13:46 -0300

    Implementation of the conditional responsible for listing all default passwords for all products from a vendor.

--- DefPass.sh
+++ DefPass.sh
@@ -2,14 +2,34 @@
 
 clear
 
-if [ "$1" == "" ] || [ "$2" == "" ]; then
-	echo "Use mode: $0 <Vendor> <Product>"
-else
-	echo "		+==================================================+"
+Banner=$(	echo "		+==================================================+"
 	echo "		|			DefPass                    |"
 	echo "		|	Anakind4, dTMP3st - Cyber Strike Force	   |"
 	echo "		|	   	     Version - 1.0                 |"
-	echo "		+==================================================+"
+	echo "		+==================================================+")
+
+if [ "$1" == "" ] || [ "$2" == "" ]; then
+	echo "$Banner"
+	echo ""
+	echo "Use mode: $0 <Vendor> <Product>"
+elif [ "$2" == "--all" ]; then
+	echo "$Banner"
+	echo ""
+	echo "Listing all standard passwords of products off $1 vendor."
+	echo ""
+	DefPassValidation=$(cat Vendors/$1/All.lst 2> /dev/null)
+	if [ "$DefPassValidation" == "" ]; then
+		echo "This product is not registered in the database."
+		# Validar possibilidade de implementar busca pela internet e em seguida implementar na base de dados.
+	else
+		for DefaultPassword in $(cat Vendors/$1/All.lst); do
+			DetectUsername=$(echo "$DefaultPassword" | cut -d "," -f1)
+			DetectPassword=$(echo "$DefaultPassword" | cut -d "," -f2)
+			echo "Username: $DetectUsername | Password: $DetectPassword"
+		done
+	fi
+else
+	echo "$Banner"
 	echo ""
 	DefPassValidation=$(cat Vendors/$1/$2.lst 2> /dev/null)
 	if [ "$DefPassValidation" == "" ]; then
